6bf4092b7ecaa29cc0f3a70cae51730cf8ee735f
johnnas12 <yohanesmesfin3@gmail.com>
2024-07-16 04:47:30 -0700

    done

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+'''Task 8's module.
+'''
+
 
 def list_all(mongo_collection):
-    '''List all documents in a collection
+    '''Lists all documents in a collection.
     '''
-
     return [doc for doc in mongo_collection.find()]
